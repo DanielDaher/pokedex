dea153a3dc20233b6d139b47dffa3e60d7705d06
Daniel <danielfdoficial@gmail.com>
2021-07-19 19:31:49 -0300

    atualiza nav-bar

--- src/App.js
+++ src/App.js
@@ -8,15 +8,14 @@ import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 function App() {
   return (
   <div className="App">
-    <h1>Pokedex</h1>
     <BrowserRouter>
       <div className="main">
         <div className='nav-bar'>
           <Link to="/" className="link">Home</Link>
+          <Link to="/pokedex" className="link">Pokedex</Link>
           <Link to="/favorites" className="link">Favorites</Link>
         </div>
       <Switch>
-        <Route exact path="/" component={Home} />
         <Route exact path="/favorites" component={Favorites} />
         <Route exact path='/pokedex' render={(props) => <Pokefile pokemons={pokemons} /> } />
       </Switch>
